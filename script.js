d86c676e9efd97e5d60dff13dd9daf8bc30397d5
Miguel Nieto <38325527+MiguelNieto1828@users.noreply.github.com>
2024-05-19 18:27:34 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    var menu = document.querySelector('.navbar .menu');
+    if (menu.style.display === 'flex') {
+        menu.style.display = 'none';
+    } else {
+        menu.style.display = 'flex';
+    }
+}
